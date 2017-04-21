fb145cc72bd642c82b4dd32b62829dcceaa6ed9b
erwin velasquez <erwinvelasquez@erwins-MacBook-Pro.local>
2017-04-20 19:51:58 -0700

    addedscoreboard

--- main.js
+++ main.js
@@ -29,79 +29,110 @@ $('.square').click(function(){
         $(this).text('O')
     }
      counter +=1  
-     checkwin() 
+    if (checkwin()) {
+       $('.square').text("-") 
+       counter=1
+    }
 })
 
 function checkwin(){
     if ($('#square00').text()==='X' &&$('#square01').text()==='X'&& $('#square02').text()==='X' ){
         alert('Player 1 Wins');
-        return 
+        increaseScores(1)
+        return true
     }
     if ($('#square00').text()==='O' &&$('#square01').text()==='O'&& $('#square02').text()==='O' ){
         alert('Player 2 Wins');
-        return 
+        increaseScores(2)
+        return  true
     }
     if ($('#square10').text()==='X' &&$('#square11').text()==='X'&& $('#square12').text()==='X' ){
         alert('Player 1 Wins');
-        return 
+        increaseScores(1)
+        return  true
     }
     if ($('#square10').text()==='O' &&$('#square11').text()==='O'&& $('#square12').text()==='O' ){
         alert('Player 2 Wins');
-        return 
+        increaseScores(2)
+                
+        return  true
     }
     if ($('#square20').text()==='X' &&$('#square21').text()==='X'&& $('#square22').text()==='X' ){
         alert('Player 1 Wins');
-        return 
+        increaseScores(1)
+        return  true
     }
     if ($('#square20').text()==='O' &&$('#square21').text()==='O'&& $('#square22').text()==='O' ){
         alert('Player 2 Wins');
-        return 
+        increaseScores(2)
+        return  true
     }
     if ($('#square00').text()==='X' &&$('#square10').text()==='X'&& $('#square20').text()==='X' ){
         alert('Player 1 Wins');
-        return 
+        increaseScores(1)
+        return  true
     }
     if ($('#square00').text()==='O' &&$('#square10').text()==='O'&& $('#square20').text()==='O' ){
         alert('Player 2 Wins');
-        return 
+        increaseScores(2)
+        return  true
     }
     if ($('#square01').text()==='X' &&$('#square11').text()==='X'&& $('#square21').text()==='X' ){
         alert('Player 1 Wins');
-        return 
+        increaseScores(1)
+        return  true
     }
     if ($('#square01').text()==='O' &&$('#square11').text()==='O'&& $('#square21').text()==='O' ){
         alert('Player 2 Wins');
-        return 
+        increaseScores(2)
+        return  true
     }
     if ($('#square02').text()==='X' &&$('#square12').text()==='X'&& $('#square22').text()==='X' ){
         alert('Player 1 Wins');
-        return 
+        increaseScores(1)
+        return  true
     }
     if ($('#square02').text()==='O' &&$('#square12').text()==='O'&& $('#square22').text()==='O' ){
         alert('Player 2 Wins');
-        return 
+        increaseScores(2)
+        return  true
     }
     if ($('#square02').text()==='X' &&$('#square11').text()==='X'&& $('#square20').text()==='X' ){
         alert('Player 1 Wins');
-        return 
+        increaseScores(1)
+        return  true
     }
     if ($('#square02').text()==='O' &&$('#square11').text()==='O'&& $('#square20').text()==='O' ){
         alert('Player 2 Wins');
-        return 
+        increaseScores(2)
+        return  true
     }
     if ($('#square00').text()==='X' &&$('#square11').text()==='X'&& $('#square22').text()==='X' ){
         alert('Player 1 Wins');
-        return 
+        increaseScores(1)
+        return  true
     }
     if ($('#square00').text()==='O' &&$('#square11').text()==='O'&& $('#square22').text()==='O' ){
         alert('Player 2 Wins');
-        return 
+        increaseScores(2)
+        return  true
     }
     if (counter===10){
         alert ('Cats Game')
+        return true
     }
+    return false
 }
 
+function increaseScores(winner){
+    if (winner===1){
+        var $player=$('#player1score')
+        $player.html(parseInt($player.html())+1)
+    } else if (winner===2){
+        var $player2=$('#player2score')
+        $player2.html(parseInt($player2.html())+1)
+    }
+}
 
 
 
